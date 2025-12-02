ea158bfda80a096f5d8bcf2a34f279f487c73c2f
Alex-Oduor <alexoduor183@gmail.com>
2025-12-02 19:27:57 +0300

    Created a blog app

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from blog.models import blogContent
+
+# Register your models here.
+admin.site.register(blogContent)
+
---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class blogContent(models.Model):
+    date_created = models.DateField()
+    title = models.CharField(max_length = 100)
+    contents = models.TextField()
+    
+
+    def __str__(self):
+        return self.title
+
+
