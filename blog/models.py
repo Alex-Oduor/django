63e4e69740f794db3efc52191a09935f5282416e
Alex-Oduor <alexoduor183@gmail.com>
2025-12-12 16:23:34 +0300

    configured external database

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class blogContent(models.Model):
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True)
     title = models.CharField(max_length = 100)
     contents = models.TextField()
     
